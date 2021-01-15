3bf4fb0f6cca957ab02340f72351c6de98fb7ce0
wuwenqing <wuwenqing@ict.ac.cn>
2021-01-15 15:27:59 +0800

    Delete receiver_driver

--- HCMonitor/receiver_driver/init_pcie_mem_cfg.sh
+++
@@ -1,7 +0,0 @@
-mount /dev/sda /mnt/ssd
-cd pcie_driver
-sh hmtt_pcie_driver_load
-cd ../memory_dev
-sh memory_dev_load
-cd ../fpga_config
-./fpga_config
--- HCMonitor/receiver_driver/trace_operation/result1/analyze_all.sh
+++
@@ -1,6 +0,0 @@
-for((i=62; i<=64; i++)) 
-do
-	./trace_analyze_addr_wzp tmp$i.trace tmp$i.trace.analyzed 0 > 	tmp$i.trace.analyzed.tmp
-	cat tmp$i.trace.analyzed.tmp | awk '{ print $5 }' > tmp$i.trace.analyzed
-	rm -f tmp$i.trace.analyzed.tmp 
-done
